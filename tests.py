764bb1ddb4d8c67f86fc154681d1000638313ddc
CtnDev <cetinomrum@gmail.com>
2022-08-07 20:29:52 +0300

    More modifications

--- tests.py
+++ tests.py
@@ -2,4 +2,4 @@ from hello import app
 with app.test_client() as c:
     response = c.get('/')
     assert response.data == b'Hello World!'
-    assert response.status_code == 200
+    assert response.status_code == 400
